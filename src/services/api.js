825a1d7d3a2652d4513e23452ec9fee1d17fd860
vtr <vrigolon@gmail.com>
2020-03-29 19:09:05 +0100

    frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://be-the-hero-app2.herokuapp.com/'
+  baseURL: 'http://be-the-hero-app2.herokuapp.com:80'
 })
 
 export default api
